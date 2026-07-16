e0a2037db764288c081de718cc6de9cc3658431f
Kangattia <joelkangattia@gmail.com>
2026-07-16 23:42:41 +0100

    Add files via upload

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import "./globals.css";
+import { Providers } from "./providers";
 
 export const metadata = {
   title: "Quizverse",
@@ -13,7 +14,9 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }

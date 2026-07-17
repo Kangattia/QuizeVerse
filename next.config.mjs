49b7920c777f100e1d1196e4510b70d6e478baf4
Kangattia <joelkangattia@gmail.com>
2026-07-17 08:14:08 +0100

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { webpack }) => {
+    // @coinbase/onchainkit pulls in an optional "x402" payments module that
+    // isn't fully published as installable packages. We don't use x402
+    // payment features in Quizverse, so tell webpack to skip trying to
+    // bundle it instead of failing the whole build over an unused feature.
+    config.plugins.push(
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^@x402\//,
+      })
+    );
+    return config;
+  },
+};
 
 export default nextConfig;
